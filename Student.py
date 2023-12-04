a518ae389297b16681e8fa5ac91ab67983f2d6c9
KacperK1905 <147401240+KacperK1905@users.noreply.github.com>
2023-12-04 16:09:49 +0100

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Student:
+    def __init__(self, name: str, marks: List):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self) -> bool:
+        avg = 0
+        for mark in self.marks:
+            avg += mark
+
+        avg = avg / len(self.marks)
+
+        if avg > 50:
+            return True
+        else:
+            return False
+
+
