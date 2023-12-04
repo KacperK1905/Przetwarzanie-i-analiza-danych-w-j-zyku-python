c8602bbca89a9cb3b0774a2e364997880ef19fd7
KacperK1905 <147401240+KacperK1905@users.noreply.github.com>
2023-12-04 16:11:48 +0100

    Add files via upload

---
+++ zad_1.py
@@ -0,0 +1,6 @@
+def return_full_name(name: str, surname: str) -> str:
+    final_str = f'Czesc {name} {surname}!'
+    return final_str
+
+
+
---
+++ zad_5.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+def s_func(list_of_num: List[int], number: int) -> bool:
+    if number in list_of_num:
+        return True
+    else:
+        return False
